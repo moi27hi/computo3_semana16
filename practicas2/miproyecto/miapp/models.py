7188cc6f81484e84d0e063cf898f1def36b0b88e
MoisesRamirez-ugb <abelinomoises@gmail.com>
2025-11-11 10:00:27 -0600

    creacion de paginas html

--- practicas2/miproyecto/miapp/models.py
+++ practicas2/miproyecto/miapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    correo = models.EmailField(unique=True)
+    telefono = models.IntegerField(unique=True)
+    dui = models.CharField(max_length=10, unique=True)
+    
+
+class Pedido(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    fecha_pedido = models.DateTimeField()
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+
 # Create your models here.
--- practicas2/miproyecto/miapp/views.py
+++ practicas2/miproyecto/miapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Cliente, Pedido
+
+def listclientes(request):
+    clientes = Cliente.objects.all()
+    return render(request, "clientes.html", {"clientes": clientes})
+    
 
 # Create your views here.
